219ca666c69e3630d525ad48e5d5fa9634a3c71e
khantlinnyan <khantlinnyan011027@gmail.com>
2025-07-25 18:59:51 +0630

    fix: remove test section

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,9 @@ import VisionSectaion from "@/components/Home/VisionSection";
 import EventSection from "@/components/Home/EventSection";
 import { Testmonials } from "@/components/Home/Testminoals";
 import Achieved from "@/components/Home/Achieved";
-import { EventCard } from "@/components/common/EventCard";
 import { ClassSection } from "@/components/Home/ClassSection";
 import ClassRegister from "@/components/Home/ClassRegister";
-import TestSection from "@/components/common/TestSection";
 import ScrollVelocity from "@/components/ui/scroll-velocity";
-
-const P = ({ text }: { text: string }) => (
-  <p className="text-4xl ml-10 text-primary-bee-black font-monos leading-snug tracking-tight font-medium w-5/6 mb-12">
-    {text}
-  </p>
-);
 
 export default function Home() {
   return (
@@ -31,7 +23,6 @@ export default function Home() {
       </div>
       <ClassRegister />
 
-      {/* <TestSection /> */}
       <ClassSection />
       <Achieved />
       <AboutSection />
@@ -41,27 +32,4 @@ export default function Home() {
       <Testmonials />
     </main>
   );
-}
-
-{
-  /* <div className="px-3 container mx-auto grid grid-cols-12 h-4/6">
-          <div className="flex flex-col justify-end col-span-8 ">
-            <P text="The Hive supports youth with learning resources, webinar programs, and a welcoming community."></P>
-            <Link
-              href="/"
-              className=" font-medium ml-8 text-lg border border-secondary-dark-blue w-fit px-6 py-3 rounded-full text-secondary-dark-blue"
-            >
-              Join our community
-            </Link>
-          </div>
-          <div className="w-full col-span-4 flex flex-col pr-32 justify-end items-center">
-            <Image
-              src={logo}
-              alt="TheHiveLogo"
-              width={300}
-              height={300}
-              className=""
-            />
-          </div>
-        </div> */
 }
